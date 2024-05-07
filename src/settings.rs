14f7caf43f592f19c59158739cf9aed89e8347e9
AppearedOne <cubase1@gmx.ch>
2024-05-07 23:12:41 +0200

    Add structure for settings

--- src/settings.rs
+++ src/settings.rs
@@ -1,1 +1,15 @@
+use anyhow::*;
+use serde_derive::*;
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Settings {
+    pub theme: String,
+    pub favourite_samples: Vec<String>,
+}
 
+pub fn load_from_file(path: String) -> Settings {
+    todo!();
+}
+
+pub fn save_to_file(path: String, settings: &Settings) -> Result<(), anyhow::Error> {
+    todo!();
+}
